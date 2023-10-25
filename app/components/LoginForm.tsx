22d4b2591585da7e84835de4da870e218bba383f
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-10-25 22:27:30 +0200

    Connect login with backend

--- app/components/LoginForm.tsx
+++ app/components/LoginForm.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { login } from '@/services/auth';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-type TUserLoginData = {
+export type TUserLoginData = {
   username: string;
   password: string;
 };
 
 export default function LoginForm() {
+  const router = useRouter();
+  const [error, setError] = useState<boolean>(false);
   const [userLoginData, setUserLoginData] = useState<TUserLoginData>({
     username: '',
     password: '',
@@ -18,11 +22,21 @@ export default function LoginForm() {
     setUserLoginData({ ...userLoginData, [e.target.name]: e.target.value });
   };
 
-  const handleFormSubmit = (e: React.FormEvent) => {
+  const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    //TODO: Make auth API call
-    //reset state data
-    //redirect to home page if true
+    try {
+      const response = await login(userLoginData);
+      localStorage.setItem('accesToken', response.data);
+      setUserLoginData({
+        username: '',
+        password: '',
+      });
+      setError(false);
+      router.refresh();
+      router.push('/');
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
@@ -31,11 +45,15 @@ export default function LoginForm() {
       className='w-1/3 py-10 px-14 flex flex-col items-center justify-center bg-white rounded-lg space-y-10 shadow-lg'
     >
       <h1 className='text-3xl font-bold text-center'>Login</h1>
-      <div className='w-full'>
+      <div className='w-full space-y-1'>
         <label htmlFor='username' className='text-sm px-1'>
           Username
         </label>
-        <div className='flex p-2 items-center space-x-2 border-b-2 border-[#B4B4B3]'>
+        <div
+          className={`flex p-2 items-center space-x-2 border-b-2 ${
+            error ? 'border-red-600' : 'border-[#B4B4B3]'
+          }`}
+        >
           <Image src='/assets/user.png' alt='user' height={16} width={16} />
           <input
             id='username'
@@ -47,11 +65,15 @@ export default function LoginForm() {
           />
         </div>
       </div>
-      <div className='w-full'>
+      <div className='w-full space-y-1'>
         <label htmlFor='password' className='text-sm px-1'>
           Password
         </label>
-        <div className='flex p-2 items-center space-x-2 border-b-2 border-[#B4B4B3]'>
+        <div
+          className={`flex p-2 items-center space-x-2 border-b-2 ${
+            error ? 'border-red-600' : 'border-[#B4B4B3]'
+          }`}
+        >
           <Image
             src='/assets/padlock.png'
             alt='padlock'
@@ -66,6 +88,11 @@ export default function LoginForm() {
             className='w-full outline-none'
             onChange={handleInputChange}
           />
+        </div>
+        <div className='w-full'>
+          {error && (
+            <p className='text-red-600 text-sm'>Incorrect credentials</p>
+          )}
         </div>
       </div>
       <button className='w-full py-2 flex items-center justify-center bg-blue-600 rounded-md text-white uppercase hover:bg-blue-700'>
