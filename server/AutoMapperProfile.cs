b35694b769ff7372511872c35e19a76b9f66eea5
Faris101 <doricfaris50@gmail.com>
2023-10-27 15:30:24 +0200

    feat:automapper_conflict_fix

--- server/AutoMapperProfile.cs
+++ server/AutoMapperProfile.cs
@@ -13,14 +13,11 @@ public class AutoMapperProfile : Profile
         CreateMap<Project, GetProjectDto>();
         CreateMap<AddProjectDto, Project>();
         CreateMap<UpdateProjectDto, Project>();
-<<<<<<< HEAD
         CreateMap<AddSectionDto, Section>();
         CreateMap<Section, GetSectionDto>();
         CreateMap<UpdateSectionDto, Section>();
         CreateMap<Block, GetBlockDto>();
         CreateMap<AddBlockDto, Block>();
         CreateMap<UpdateBlockDto, Block>();
-=======
->>>>>>> 9e373a9bd25e8b1d8fe6ee9bb6bb75cb6879095e
     }
 }
