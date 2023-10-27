fe8be0819fbd1400e5ace8639f80417842462902
Faris101 <doricfaris50@gmail.com>
2023-10-27 15:25:29 +0200

    feat:project_conflict_fix

--- server/Controllers/ProjectController.cs
+++ server/Controllers/ProjectController.cs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-using Microsoft.AspNetCore.Authorization;
-=======
->>>>>>> 9e373a9bd25e8b1d8fe6ee9bb6bb75cb6879095e
 using Microsoft.AspNetCore.Mvc;
 using server.Dtos.ProjectDtos;
 using server.Services.ProjectService;
@@ -27,27 +23,19 @@ namespace server.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public async Task<ActionResult<GetProjectDto>> GetById(string id)
+        public async Task<ActionResult<GetProjectDto>> GetById(int id)
         {
             return Ok(await _projectService.GetProjectById(id));
         }
 
         [HttpPost]
-<<<<<<< HEAD
         public async Task AddProject(AddProjectDto newProject)
         {
             await _projectService.AddProject(newProject);
-=======
-        public async Task<ActionResult> AddProject(AddProjectDto newProject)
-        {
-            await _projectService.AddProject(newProject);
-            return NoContent();
->>>>>>> 9e373a9bd25e8b1d8fe6ee9bb6bb75cb6879095e
         }
 
         [HttpDelete]
         [Route("{id}")]
-<<<<<<< HEAD
         public async Task DeleteProject(int id)
         {
             await _projectService.DeleteProject(id);
@@ -57,19 +45,6 @@ namespace server.Controllers
         public async Task UpdateProject(UpdateProjectDto updatedProject)
         {
             await _projectService.UpdateProject(updatedProject);
-=======
-        public async Task<ActionResult> DeleteProject(string id)
-        {
-            await _projectService.DeleteProject(id);
-            return NoContent();
-        }
-
-        [HttpPut]
-        public async Task<ActionResult> UpdateProject(UpdateProjectDto updatedProject)
-        {
-            await _projectService.UpdateProject(updatedProject);
-            return NoContent();
->>>>>>> 9e373a9bd25e8b1d8fe6ee9bb6bb75cb6879095e
         }
 
     }
