83471fa5406f9562e3d2d1121664b57b1163efd7
Faris101 <doricfaris50@gmail.com>
2023-10-27 15:15:10 +0200

    Merge branch 'main' into block-backend

--- server/Program.cs
+++ server/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddCors(options =>
     });
 });
 
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
