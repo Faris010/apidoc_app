42a38fa3987ab52ad28286e8f243e197d2f6c2b3
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-11-10 01:30:41 +0100

    server fix

--- server/Dtos/SectionDtos/UpdateSectionDto.cs
+++ server/Dtos/SectionDtos/UpdateSectionDto.cs
@@ -2,6 +2,7 @@ namespace server.Dtos.SectionDtos;
 
 public class UpdateSectionDto
 {
+    public required Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public Guid ProjectId { get; set; }
