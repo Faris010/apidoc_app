2cfaa800bfbf05a0ec637055ad2109e6900b77ca
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-11-12 01:15:26 +0100

    update: Update block components
    -Move logic into components

--- app/components/block_type/CodeBlock.tsx
+++ app/components/block_type/CodeBlock.tsx
@@ -1,19 +1,50 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import CodeEditor from '@uiw/react-textarea-code-editor';
+import { TBLock } from '@/types/types';
+import useDebounce from '@/hooks/useDebounce';
+import { editBlock } from '@/services/block';
 
-const code = "console.log('Code Mirror!');";
+interface Props {
+  block: TBLock;
+}
 
-const CodeBlock = () => {
+const CodeBlock = ({ block }: Props) => {
+  const [codeBlockValue, setCodeBlockValue] = useState<string | null>(
+    block?.content || null
+  );
+
+  const debouncedValue = useDebounce(codeBlockValue, 1500);
+
+  const updateCode = async (updatedContent: string) => {
+    const updatedBlock = { ...block, content: updatedContent };
+    await editBlock(updatedBlock);
+  };
+
+  useEffect(() => {
+    if (debouncedValue != null && debouncedValue != block?.content) {
+      updateCode(debouncedValue);
+      console.log('Update');
+    }
+  }, [debouncedValue]);
+
+  const handleCodeBlockChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCodeBlockValue(e.target.value);
+  };
+
   return (
-    <div className='w-full overflow-hidden rounded'>
+    <div data-color-mode='light' className='w-full overflow-hidden rounded'>
       <CodeEditor
-        value={code}
+        value={codeBlockValue || ''}
         language='js'
+        placeholder='Please enter code.'
+        onChange={handleCodeBlockChange}
         padding={15}
         style={{
-          fontSize: 12,
-          backgroundColor: '#f5f5f5',
           fontFamily:
             'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+          fontSize: 12,
         }}
       />
     </div>
