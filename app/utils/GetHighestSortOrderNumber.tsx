8d7911d2fd034c157f44a5722a1d4bdc8f79423b
MuhammedSarajlic <muhammed.sarajlic.20@size.ba>
2023-11-12 01:13:12 +0100

    feat: Export highest sort order fucntion

---
+++ app/utils/GetHighestSortOrderNumber.tsx
@@ -0,0 +1,11 @@
+import { TBLock } from '@/types/types';
+
+export function getHighestSortOrderNumber(blockList: TBLock[]): number {
+  if (blockList.length === 0) {
+    return 0;
+  }
+
+  return blockList.reduce((maxSortOrder, block) => {
+    return Math.max(maxSortOrder, block.sortOrder || 0);
+  }, 0);
+}
